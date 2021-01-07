c5b339c07e6da403463e4c33862d1416b3cc49f7
1394469939 <71317498+1394469939@users.noreply.github.com>
2021-01-07 18:07:16 +0800

    Add files via upload

---
+++ 作业十/src/作业十/e2.java
@@ -0,0 +1,23 @@
+package ×÷ÒµÊ®;
+import java.io.*;
+public class e2 {
+	public static void main(String[] args)throws IOException
+	{
+		File in=new File("/Users/lihaji/Desktop/in.txt");
+		File out=new File("/Users/lihaji/Desktop/out.txt");
+		try
+		{
+			FileInputStream cin=new FileInputStream(in);
+			FileOutputStream cout=new FileOutputStream(out);
+			int a;
+			while((a=cin.read())!=-1)
+			{
+				cout.write((char)a);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error");
+		}
+	}
+}
